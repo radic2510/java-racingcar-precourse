4e4dd3e82bcee1f2369c24a2b3b3322ffe39b3cd
Jeayoeb Kim <jykim@atones.co.kr>
2022-04-25 03:27:32 +0900

    feat: ERROR 발생 시 에러 메시지 출력 후 다시 입력 받기 기능 추가

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -12,15 +12,33 @@ public class RaceController {
     }
 
     public void raceStart() {
+        Cars cars = getCarsWithUserInput();
+        int roundNumber = getRoundNumberWithUserInput();
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Cars cars = new Cars(consoleHandler.readCarNames());
-
-        System.out.println("시도할 회수는 회인가요?");
-        int roundNumberInput = consoleHandler.readRoundNumber();
-
-        runRound(cars, roundNumberInput);
+        runRound(cars, roundNumber);
         raceResult(cars);
+    }
+
+    private Cars getCarsWithUserInput() {
+        while (true) {
+            try {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                return new Cars(consoleHandler.readCarNames());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int getRoundNumberWithUserInput() {
+        while (true) {
+            try {
+                System.out.println("시도할 회수는 회인가요?");
+                return consoleHandler.readRoundNumber();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private void runRound(Cars cars, int roundNumberInput) {
